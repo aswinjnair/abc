44ba1a5190d0af7c7c9cb4ef0a296d4e1bc20f41
aswinjnair <aswinjayan1992@gmail.com>
2017-05-14 02:16:02 +0530

    changes

--- index.js
+++ index.js
@@ -1,22 +1,66 @@
-var express = require('express');  
-var bodyParser = require('body-parser');  
-var request = require('request');  
-var app = express();
+'use strict'
 
-app.use(bodyParser.urlencoded({extended: false}));  
-app.use(bodyParser.json());  
-app.listen((process.env.PORT || 3000));
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
 
-// Server frontpage
-app.get('/', function (req, res) {  
-    res.send('This is TestBot Server');
-});
+const app = express()
 
-// Facebook Webhook
-app.get('/webhook', function (req, res) {  
-    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
-        res.send(req.query['hub.challenge']);
-    } else {
-        res.send('Invalid verify token');
-    }
-});
+app.set('port', (process.env.PORT || 3000))
+
+// Allows us to process the data
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// ROUTES
+
+app.get('/', function(req, res) {
+	res.send("Hi I am a chatbot")
+})
+
+let token = "EAAafRi5SJtUBAK7wlZCgAXrlLeUZBW4iDlowKkAZAmoBkHruTrQeLkZABjyaRoFxT2PZAxpHEy8VBX1LlX84iZC7HZCyUHhZCKcZCgSzaaZCnZAZCjm5ZAXqBgnAnywZAhnFe0Ce4H2OFcfjapvLsWRehprqZAx8zsj4v0he83JjGHfYTrZBzyacmRjAKkgr"
+
+// Facebook 
+
+app.get('/webhook/', function(req, res) {
+	if (req.query['hub.verify_token'] === "testbot_verify_token") {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send("Wrong token")
+})
+
+app.post('/webhook/', function(req, res) {
+	let messaging_events = req.body.entry[0].messaging
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = messaging_events[i]
+		let sender = event.sender.id
+		if (event.message && event.message.text) {
+			let text = event.message.text
+			sendText(sender, "Text echo: " + text.substring(0, 100))
+		}
+	}
+	res.sendStatus(200)
+})
+
+function sendText(sender, text) {
+	let messageData = {text: text}
+	request({
+		url: "https://graph.facebook.com/v2.6/me/messages",
+		qs : {access_token: token},
+		method: "POST",
+		json: {
+			recipient: {id: sender},
+			message : messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log("sending error")
+		} else if (response.body.error) {
+			console.log("response body error")
+		}
+	})
+}
+
+app.listen(app.get('port'), function() {
+	console.log("running: port")
+})
